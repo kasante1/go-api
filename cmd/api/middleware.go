3b63e4607e83adca66101e871a462417fae5d034
ds asante <73370911+kasante1@users.noreply.github.com>
2024-09-11 18:52:48 +0000

    ft: rate limiter middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"golang.org/x/time/rate"
 ) 
 
 
@@ -20,4 +21,17 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	next.ServeHTTP(w, r)
 })
 
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	if !limiter.Allow() {
+		app.rateLimitExceededResponse(w, r)
+		return
+		}
+	})
 }
