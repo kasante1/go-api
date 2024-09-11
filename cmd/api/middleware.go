b519361d26fa1a2fb766d8a6a0ee0d6062970f67
ds asante <73370911+kasante1@users.noreply.github.com>
2024-09-11 19:37:29 +0000

    ft: limiters ip based

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,12 +1,16 @@
 package main
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync" 
 	"golang.org/x/time/rate"
+	"time" 
 ) 
 
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 	
@@ -24,14 +28,56 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
+	type client struct {
+		limiter *rate.Limiter
+		lastSeen time.Time
+		}
 
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		mu sync.Mutex
+		clients = make(map[string]*client)
+		)
 
+		go func() {
+			for {
+				time.Sleep(time.Minute)
+			
+				mu.Lock()
+				
+				for ip, client := range clients {
+
+					if time.Since(client.lastSeen) > 3*time.Minute {
+						delete(clients, ip)
+						}
+
+					}
+					mu.Unlock()
+			}
+		}()
+		
+			
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-	if !limiter.Allow() {
-		app.rateLimitExceededResponse(w, r)
-		return
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
 		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		mu.Unlock()
+		next.ServeHTTP(w, r)
 	})
 }
