02ace638ec0f54b8ff980ed53fa68971701e53f6
ds asante <73370911+kasante1@users.noreply.github.com>
2024-09-16 00:06:53 +0000

    ft: grant permission

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -1,19 +1,20 @@
 package data
 import (
-"context"
-"database/sql"
-"time"
+	"context"
+	"database/sql"
+	"time"
+	"github.com/lib/pq"
 )
 
 type Permissions []string
 
 func (p Permissions) Include(code string) bool {
-for i := range p {
-if code == p[i] {
-return true
-}
-}
-return false
+	for i := range p {
+		if code == p[i] {
+			return true
+		}
+	}
+		return false
 }
 
 type PermissionModel struct {
@@ -52,4 +53,17 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 		return nil, err
 		}
 		return permissions, nil
+}
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
